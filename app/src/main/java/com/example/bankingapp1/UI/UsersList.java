0f4a759a107ee58b218320889941afa444d4d71f
Shagufta Hundekari <shagufta.hundekari01@gmail.com>
2024-01-09 01:51:38 -0800

    Banking System App

---
+++ app/src/main/java/com/example/bankingapp1/UI/UsersList.java
@@ -0,0 +1,103 @@
+package com.example.bankingapp1.UI;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.bankingapp1.DB.TransactionContract;
+import com.example.bankingapp1.DB.UserContract;
+import com.example.bankingapp1.DB.UserHelper;
+import com.example.bankingapp1.Data.User;
+import com.example.bankingapp1.ListAdapters.UserListAdapter;
+import com.example.bankingapp1.R;
+import com.example.bankingapp1.DB.UserContract.UserEntry;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+public class UsersList extends AppCompatActivity {
+    // RecyclerView
+    RecyclerView recyclerView;
+    RecyclerView.Adapter myAdapter;
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<User> userArrayList;
+
+    // Database
+    private UserHelper dbHelper;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_list);
+
+        // Create ArrayList of Users
+        userArrayList = new ArrayList<User>();
+
+        // Create Table in the Database
+        dbHelper = new UserHelper(this);
+
+        // Read Data from DataBase
+        displayDatabaseInfo();
+
+        // Show list of items
+        recyclerView = findViewById(R.id.all_users_list);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        myAdapter = new UserListAdapter(this, userArrayList);
+        recyclerView.setAdapter(myAdapter);
+
+
+        btn=findViewById(R.id.button_user);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(UsersList.this,HomeScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    private void displayDatabaseInfo() {
+        userArrayList.clear();
+
+        Cursor cursor = new UserHelper(this).readAllData();
+
+        int phoneNoColumnIndex = cursor.getColumnIndex(UserEntry.COLUMN_USER_PHONE_NO);
+        int emailColumnIndex = cursor.getColumnIndex(UserEntry.COLUMN_USER_EMAIL);
+        int ifscCodeColumnIndex = cursor.getColumnIndex(UserEntry.COLUMN_USER_IFSC_CODE);
+        int accountNumberColumnIndex = cursor.getColumnIndex(UserEntry.COLUMN_USER_ACCOUNT_NUMBER);
+        int nameColumnIndex = cursor.getColumnIndex(UserEntry.COLUMN_USER_NAME);
+        int accountBalanceColumnIndex = cursor.getColumnIndex(UserEntry.COLUMN_USER_ACCOUNT_BALANCE);
+
+        while (cursor.moveToNext()){
+            String currentName = cursor.getString(nameColumnIndex);
+            int accountNumber = cursor.getInt(accountNumberColumnIndex);
+            String email = cursor.getString(emailColumnIndex);
+            String phoneNumber = cursor.getString(phoneNoColumnIndex);
+            String ifscCode = cursor.getString(ifscCodeColumnIndex);
+            int accountBalance = cursor.getInt(accountBalanceColumnIndex);
+
+            // Display the values from each column of the current row in the cursor in the TextView
+            userArrayList.add(new User(currentName, accountNumber, phoneNumber, ifscCode, accountBalance, email));
+        }
+    }
+}
